3a0c1fc690c3ce0ca983726f05ff382fa06f75ff
Konstantin Shamiev <konstantin@shamiev.ru>
2015-11-24 16:35:35 +0300

    feature layotu to two

--- hostkey_com_new/application/shop/class/Domains/Page.php
+++ hostkey_com_new/application/shop/class/Domains/Page.php
@@ -12,6 +12,25 @@
 class Shop_Domains_Page extends Zero_Controller
 {
     /**
+     * The compile tpl in string and out
+     *
+     * @var bool
+     */
+    protected $ViewTplOutString = true;
+
+    /**
+     * Контроллер по умолчанию.
+     *
+     * @return Zero_View
+     */
+    public function Action_Default()
+    {
+        $this->Chunk_Init();
+        $this->Chunk_View();
+        return $this->View->Fetch($this->ViewTplOutString);
+    }
+
+    /**
      * Фабричный метод по созданию контроллера.
      *
      * @param array $properties входные параметры плагина
