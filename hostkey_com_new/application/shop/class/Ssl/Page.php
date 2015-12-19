84ef60d8befd8382675671b0beca5f87b1f47672
Konstantin Shamiev <konstantin@shamiev.ru>
2015-12-19 18:34:25 +0300

    feature

--- hostkey_com_new/application/shop/class/Ssl/Page.php
+++ hostkey_com_new/application/shop/class/Ssl/Page.php
@@ -12,6 +12,31 @@
 class Shop_Ssl_Page extends Zero_Controller
 {
     /**
+     * Контроллер по умолчанию.
+     *
+     * @return Zero_View
+     */
+    public function Action_Default()
+    {
+        $this->Chunk_Init();
+        $this->Chunk_View();
+        return $this->View;
+    }
+
+    /**
+     * Вывод данных операции контроллера в шаблон
+     *
+     * Может быть переопределен конкретным контроллером
+     *
+     * @return bool
+     */
+    protected function Chunk_View()
+    {
+        $this->View->Assign('Content', Zero_App::$Section->Content);
+        return true;
+    }
+
+    /**
      * Фабричный метод по созданию контроллера.
      *
      * @param array $properties входные параметры плагина
