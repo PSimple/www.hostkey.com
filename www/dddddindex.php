8ba8c4be262e4df65a70de1d0980b9488c50453f
Konstantin Shamiev <konstantin@shamiev.ru>
2016-04-28 19:42:59 +0400

    deploy

---
+++ www/dddddindex.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The entry point to the application.
+ * Initialize and run.
+ */
+
+// Including the class Zero_App
+require __DIR__ . '/zero/class/App.php';
+
+Zero_App::Init();
+
+// General Authorization Application
+if ( Zero_App::$Config->Site_AccessLogin )
+{
+    if ( !isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != Zero_App::$Config->Site_AccessLogin || $_SERVER['PHP_AUTH_PW'] != Zero_App::$Config->Site_AccessPassword )
+    {
+        header('WWW-Authenticate: Basic realm="Auth"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Auth Failed';
+        exit;
+    }
+}
+
+Zero_App::Execute();
+exit;
