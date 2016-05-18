b222743a2e94431a1295bc3961060a72d30304eb
Pavel <tpsimple@gmail.com>
2016-05-18 19:15:11 +0300

    DNO-263-69091

---
+++ www/application/content/class/Section/About.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Контент страницы (раздела).
+ *
+ * @package Content.Controller.Section
+ * @author Konstantin Shamiev aka ilosa <konstantin@shamiev.ru>
+ * @date 2015.07.17
+ */
+class Content_Section_About extends Zero_Controller
+{
+    /**
+     * Контроллер по умолчанию.
+     *
+     * @return string
+     */
+    public function Action_Default()
+    {
+        $this->Chunk_Init();
+        preg_match_all('#(<.+?>)(.+?)(<\/.+?>)#is', Zero_App::$Section->Name, $match);
+        if ( false && isset($match[2][0]) )
+        {
+            $head = str_replace(' ', '<br>', $match[2][0]);
+            pre($match[1][0] . $head . $match[3][0]);
+            $this->View->Assign('head', $match[1][0] . $head . $match[3][0]);
+        }
+        else
+        {
+            $this->View->Assign('head', Zero_App::$Section->Name);
+        }
+        $this->View->Assign('HEADER_CONTENT', Zero_App::$Section->NameSub);
+        $this->View->Assign('content', Zero_App::$Section->Content);
+
+        return $this->View;
+    }
+
+    /**
+     * Фабричный метод по созданию контроллера.
+     *
+     * @param array $properties входные параметры плагина
+     * @return Content_Section_About
+     */
+    public static function Make($properties = [])
+    {
+        $Controller = new self();
+        foreach ($properties as $property => $value)
+        {
+            $Controller->Params[$property] = $value;
+        }
+        return $Controller;
+    }
+}
