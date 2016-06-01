dd54e02422df935475e6388afaf6c0dfd6e7703f
Pavel <tpsimple@gmail.com>
2016-06-01 13:37:30 +0300

    rebuilding api

--- www/assets/js/domains.js
+++ www/assets/js/domains.js
@@ -16,6 +16,28 @@ String.prototype.stripTags = function () {
     return this.replace(/<\/?[^>]+>/g, '');
 };
 
+function bindTableClick() {
+    $('.tab-list__content-table-row__available').on('click', 'a.tab-list__content-reg-this', function () {
+        var domain = $(this).attr('data-domain');
+        var price = $(this).attr('data-price');
+        var searchThis = reg.indexOf(domain);
+        if (searchThis !== undefined && searchThis != null) {
+            if (searchThis < 0) {
+                reg.push(domain);
+                $('#domains-register-table tbody').append(
+                    '<tr class="domains-step2__summary-table-row">' +
+                    '<td class="domains-step2__summary-table-cell">' + domain + '</td>' +
+                    '<td class="domains-step2__summary-table-cell">€' + price + '</td>' +
+                    '</tr>');
+
+                summaryPrice += parseInt(price);
+                $('#Summa').html('€' + summaryPrice);
+            }
+        }
+        return false;
+    });
+}
+
 $('#domain-zone__more').on('click', function () {
     $.getJSON('/api/v1/shop/domains/zone/list?groups=top100', function (data) {
         var items = data['Content'];
@@ -126,7 +148,7 @@ $.getJSON('/api/v1/shop/domains/zone/list?groups=promo', function (data) {
     AddData('.domains-check__row', readyPromo);
 });
 
-$('.search-bar__input').on('keyup', function (e) {
+$('.search-bar__input:visible').on('keyup', function (e) {
     var svalue = $(this).val().replace(/[^a-zA-Z0-9-^.\n]/gim, '');
     $(this).val(svalue);
 
@@ -137,93 +159,101 @@ $('.search-bar__input').on('keyup', function (e) {
 $('.search-bar .b-submit').on('click', '', function (e) {
     var $checkedInput = $('.hidden-input:checked');
     searchZones = '';
+    var readyTable = '';
+    var readyTable2 = '';
     $.each($checkedInput, function () {
         searchZones += $(this).data('name').replace('.', '') + ',';
     });
     searchZones = searchZones.slice(0, -1);
-    var searchDomains = ($('.search-bar__input').val()).replace(/\n/g, ',');
+    var searchDomains = ($('.search-bar__input:visible').val()).replace(/\n/g, ',');
     if (searchDomains.substr(-1, 1) == ',') {
         searchDomains = searchDomains.substr(0, -1);
     }
-    $.getJSON('/api/v1/shop/domains/check?domainList=' + searchDomains + '&zoneList=' + searchZones, function (data) {
-        var items = data['Content'];
-        var readyTable = '';
-        var classAv;
-        for (key in items) {
-            var status = items[key]['status'];
-            var priceReg = items[key]['priceRegister'];
-            var priceTrans = items[key]['priceTransfer'];
-            var priceRenew = items[key]['priceRenew'];
-            var priceOld = items[key]['priceOld'];
-            switch (status) {
-                case 'error':
-                case 'invalid domain':
-                    classAv = 'invalid';
-                    break;
-                case 'available':
-                    classAv = 'available';
-                    break;
-                case 'not available':
-                default:
-                    classAv = 'registred';
-                    break;
-            }
-            readyTable += '<tr class="tab-list__content-table-row tab-list__content-table-row__' + classAv + '">' +
-                '<td class="tab-list__content-table-cell">' + key + '</td>' +
-                '<td class="tab-list__content-table-cell">' + status + '</td>' +
-                '<td class="tab-list__content-table-cell">' +
-                '<a href="#" class="tab-list__content-reg-this" data-domain="' + key + '" data-price="' + priceReg + '">' +
-                '<i class="b-icon"></i>Register | <span>€' + priceReg + '</span>' +
-                '</a>' +
-                '<a href="#" class="tab-list__content-its-my" data-domain="' + key + '" data-priceTrans="' + priceTrans + '"  data-priceRenew="' + priceRenew + '" >' +
-                '<i class="b-icon"></i>It\'s my domain</a>' +
-                '</td></tr>';
-
-        }
-
-        AddData('#result-table tbody', readyTable);
-        $('.tab-list__content-table-row__available').on('click', 'a.tab-list__content-reg-this', function () {
-            var domain = $(this).attr('data-domain');
-            var price = $(this).attr('data-price');
-            var searchThis = reg.indexOf(domain);
-            if (searchThis !== undefined && searchThis != null) {
-                if (searchThis < 0) {
-                    reg.push(domain);
-                    $('#domains-register-table tbody').append(
-                        '<tr class="domains-step2__summary-table-row">' +
-                        '<td class="domains-step2__summary-table-cell">' + domain + '</td>' +
-                        '<td class="domains-step2__summary-table-cell">€' + price + '</td>' +
-                        '</tr>');
-
-                    summaryPrice += parseInt(price);
-                    $('#Summa').html('€' + summaryPrice);
+    var searchDomainsArr = searchDomains.split(',');
+    for (key in searchDomainsArr) {
+        var onedomainname = searchDomainsArr[key];
+        $.getJSON('/api/v1/shop/domains/check?domainList=' + onedomainname + '&zoneList=' + searchZones, function (data) {
+            var items = data['Content']['group1'];
+            var itemsTop = data['Content']['group2'];
+            var classAv;
+            var classAv2;
+            for (key in items) {
+                var status = items[key]['status'];
+                var priceReg = items[key]['priceRegister'];
+                var priceTrans = items[key]['priceTransfer'];
+                var priceRenew = items[key]['priceRenew'];
+                var priceOld = items[key]['priceOld'];
+                switch (status) {
+                    case 'error':
+                    case 'invalid domain':
+                        classAv = 'invalid';
+                        break;
+                    case 'available':
+                        classAv = 'available';
+                        break;
+                    case 'not available':
+                    default:
+                        classAv = 'registred';
+                        break;
                 }
+                readyTable += '<tr class="tab-list__content-table-row tab-list__content-table-row__' + classAv + '">' +
+                    '<td class="tab-list__content-table-cell">' + key + '</td>' +
+                    '<td class="tab-list__content-table-cell">' + status + '</td>' +
+                    '<td class="tab-list__content-table-cell">' +
+                    '<a href="#" class="tab-list__content-reg-this" data-domain="' + key + '" data-price="' + priceReg + '">' +
+                    '<i class="b-icon"></i>Register | <span>€' + priceReg + '</span>' +
+                    '</a>' +
+                    '<a href="#" class="tab-list__content-its-my" data-domain="' + key + '" data-priceTrans="' + priceTrans + '"  data-priceRenew="' + priceRenew + '" >' +
+                    '<i class="b-icon"></i>It\'s my domain</a>' +
+                    '</td></tr>';
+
             }
-            return false;
+            for (key in itemsTop) {
+                var status2 = itemsTop[key]['status'];
+                var priceReg2 = itemsTop[key]['priceRegister'];
+                var priceTrans2 = itemsTop[key]['priceTransfer'];
+                var priceRenew2 = itemsTop[key]['priceRenew'];
+                var priceOld2 = itemsTop[key]['priceOld'];
+                var promo = '';
+                if ('promo' in itemsTop[key])
+                    promo = 'promoRow';
+                switch (status2) {
+                    case 'error':
+                    case 'invalid domain':
+                        classAv2 = 'invalid';
+                        break;
+                    case 'available':
+                        classAv2 = 'available';
+                        break;
+                    case 'not available':
+                    default:
+                        classAv2 = 'registred';
+                        break;
+                }
+                readyTable2 += '<tr class="tab-list__content-table-row tab-list__content-table-row__' + classAv2 + promo + '">' +
+                    '<td class="tab-list__content-table-cell">' + key + '</td>' +
+                    '<td class="tab-list__content-table-cell">' + status2 + '</td>' +
+                    '<td class="tab-list__content-table-cell">' +
+                    '<a href="#" class="tab-list__content-reg-this" data-domain="' + key + '" data-price="' + priceReg2 + '">' +
+                    '<i class="b-icon"></i>Register | <span>€' + priceReg2 + '</span>' +
+                    '</a>' +
+                    '<a href="#" class="tab-list__content-its-my" data-domain="' + key + '" data-priceTrans="' + priceTrans2 + '"  data-priceRenew="' + priceRenew2 + '" >' +
+                    '<i class="b-icon"></i>It\'s my domain</a>' +
+                    '</td></tr>';
+
+            }
+            AddData('#result-table tbody', readyTable);
+            AddData('#result-table2 tbody', readyTable2);
+
+
+            bindTableClick();
         });
-        $('.domains-step').hide();
-        $('#step2').show();
-        $('html, body').animate({
-            scrollTop: $("#step2").offset().top
-        }, 2000);
-    });
-    return false;
-});
-
-
-$('.tab-list__content-table-row__available').on('click', 'a.tab-list__content-reg-this', function () {
-    var domain = $(this).attr('data-domain');
-    var price = $(this).attr('data-price');
-    var searchThis = reg.indexOf(domain);
-    if (searchThis !== undefined && searchThis != null) {
-        if (searchThis < 0) {
-            reg.push(domain);
-            $('#domains-register-table tbody').append(
-                '<tr class="domains-step2__summary-table-row">' +
-                '<td class="domains-step2__summary-table-cell">' + domain + '</td>' +
-                '<td class="domains-step2__summary-table-cell">€' + price + '</td>' +
-                '</tr>');
-        }
     }
+
+    $('.domains-step').hide();
+    $('#step2').show();
+    $('html, body').animate({
+        scrollTop: $("#step2").offset().top
+    }, 2000);
     return false;
 });
