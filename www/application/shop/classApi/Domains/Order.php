babdef362c8ed630fd4eaa288892a1cfc70a3b4e
Konstantin Shamiev <konstantin@shamiev.ru>
2016-08-10 13:01:42 +0300

    feature domain basket

--- www/application/shop/classApi/Domains/Order.php
+++ www/application/shop/classApi/Domains/Order.php
@@ -19,26 +19,36 @@ class Shop_Api_Domains_Order extends Zero_Controller
         // https://bill.hostkey.com/cart.php?a=add&currency=2&pid=564&billingcycle=monthly&configoption[858]=15006&customfield[348]=описание-заказа
 
         // Проверки
-        if ( !isset($_REQUEST['domains']) )
+        if (empty($_REQUEST['domains']) || empty($_REQUEST['dns']))
             Zero_App::ResponseJson500(-1, ["список доменов не указан 'domains'"]);
 
+        // проверка dns
+        $checkNs = [];
+        foreach ($_REQUEST['dns'] as $ns) {
+            $arr = explode('.', $ns);
+            array_shift($arr);
+            $ns = implode('.', $arr);
+            $checkNs[$ns] = 1;
+        }
+        if (1 < count($checkNs))
+            Zero_App::ResponseJson500(-1, ["ns сервера указаны не верно"]);
+
         $orderList = [];
-        foreach($_REQUEST['domains'] as $domain => $row)
-        {
+        foreach ($_REQUEST['domains'] as $domain => $row) {
             $zone = explode('.', $domain)[1];
             $orderList[$zone][$domain] = [
+                'advanced' => $row['advanced'],
                 'periodReg' => $row['periodReg'] / 12,
+                'periodTrans' => $row['periodTrans'] / 12,
+                'periodRenew' => $row['periodRenew'] / 12,
                 'dns' => $row['dns'],
             ];
         }
 
-        $result = Zero_App::RequestJson('POST', 'https://bill.hostkey.com/api/v1/billing/domains/orders', ['orderList' => $orderList]);
-        if ( $result['ErrorStatus'] == false )
-        {
+        $result = Zero_App::RequestJson('POST', 'https://bill.hostkey.com/api/v1/billing/domains/orders', ['orderList' => $orderList, 'dns' => $_REQUEST['dns']]);
+        if ($result['ErrorStatus'] == false) {
             Zero_App::ResponseJson200($result['Content']);
-        }
-        else
-        {
+        } else {
             Zero_App::ResponseJson500($result['Code'], [$result['Message']]);
         }
         return true;
@@ -53,8 +63,7 @@ class Shop_Api_Domains_Order extends Zero_Controller
     public static function Make($properties = [])
     {
         $Controller = new self();
-        foreach ($properties as $property => $value)
-        {
+        foreach ($properties as $property => $value) {
             $Controller->Params[$property] = $value;
         }
         return $Controller;
